53f753249639953e098247dc85e5c2165c96a878
Niklas Quarfot Nielsen <nik@qni.dk>
2020-06-03 20:12:39 -0700

    CSV upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var storageRef = window.firebase.storage().ref();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFileUploadSubmit = this.handleFileUploadSubmit.bind(this);
+    this.handleFileUploadChange = this.handleFileUploadChange.bind(this);
+    this.state = {
+      'selectedFile': undefined
+    };
+  }
+
+  handleFileUploadSubmit(e) {
+    const uploadTask = storageRef.child(`csvs/${this.state.selectedFile.name}`).put(this.state.selectedFile); //create a child directory called images, and place the file inside this directory
+    uploadTask.on('state_changed', (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    }, (error) => {
+      // Handle unsuccessful uploads
+      console.log(error);
+    }, () => {
+       // Do something once upload is complete
+       console.log('success');
+    });
+  }
+
+  handleFileUploadChange(e) {
+    this.setState({'selectedFile': e.target.files[0]});
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="filesubmit">
+          <input type="file" class="file-select" accept=".csv" onChange={this.handleFileUploadChange}/>
+          <button class="file-submit" onClick={this.handleFileUploadSubmit}>Upload</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
