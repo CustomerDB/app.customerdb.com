9954ba65176280455da39a6bcf0317ed1b487d3a
Connor Doyle <connor@quantap.com>
2020-06-05 20:18:26 -0700

    Minor logging changes in storage trigger

--- functions/index.js
+++ functions/index.js
@@ -7,58 +7,63 @@ const fs = require('fs');
 const csv = require('csv-parser');
 
 exports.processCSVUpload = functions.storage.object().onFinalize((object) => {
-    const fileBucket = object.bucket; // The Storage bucket that contains the file.
-    const filePath = object.name; // File path in the bucket.
+	const fileBucket = object.bucket; // The Storage bucket that contains the file.
+	const filePath = object.name; // File path in the bucket.
 
-    // Get the file name.
+	// Get the file name.
 	const datasetID = path.basename(filePath);
 
-    const bucket = admin.storage().bucket(fileBucket);
+	const bucket = admin.storage().bucket(fileBucket);
 	const tempFilePath = path.join(os.tmpdir(), datasetID);
 
-    let db = admin.firestore();
+	let db = admin.firestore();
+	let dataset = db.collection('datasets').doc(datasetID);
+	let highlights = dataset.collection('highlights');
 
 	// Read the CSV.
 	let batch = db.batch();
 	let batchSize = 250;  // batched writes can contain up to 500 operations
 	let batchPromises = [];
 	let count = 0;
+	let batchNum = 1;
 
 	let download = bucket.file(filePath).download({destination: tempFilePath});
 	return download.then(function() {
 		return new Promise(function (resolve, reject) {
 			console.log("read csv")
+
 			fs.createReadStream(tempFilePath)
-			.pipe(csv())
-			.on('data', function(row) {
-				let highlightRef = db.collection('datasets').doc(datasetID).collection('highlights').doc();
-				batch.set(highlightRef, row);
-				count++;
-			
-				if (count == batchSize) {
-					console.log("Commit #1!");
+				.pipe(csv())
+				.on('data', function(row) {
+					let highlightRef = highlights.doc();
+					batch.set(highlightRef, row);
+					count++;
+
+					if (count == batchSize) {
+						console.log(`Batch commit ${batchNum}`);
+						batchPromises.push(batch.commit());
+						batch = db.batch();
+						count = 0;
+						batchNum++;
+					}
+				})
+				.on('end', () => {
+					console.log('Done processing CSV file');
 					batchPromises.push(batch.commit());
-					batch = db.batch();
-					count = 0;
-				}
-				console.log('<< row');
-			})
-			.on('end', () => {
-				console.log('Done processing CSV file');
-				resolve(Promise.all(batchPromises));
-			});
+					resolve(Promise.all(batchPromises));
+				});
 		})
-	}).then(function () {
-		return batch.commit()
-	}).then(function() {
-        console.log("Recording processed timestamp");
-        let now = new Date();
-        return db.collection('datasets').doc(datasetID).set({
-          processedAt: now.toISOString()
-        }, {merge: true});
-    }).then(function() {
-        console.log("Deleting temporary file");
-        return fs.unlinkSync(tempFilePath);
-    })
-  }
+	})
+	.then(function() {
+		console.log("Recording processed timestamp");
+		let now = new Date();
+		return dataset.set({
+			processedAt: now.toISOString()
+		}, {merge: true});
+	})
+	.then(function() {
+		console.log("Deleting temporary file");
+		return fs.unlinkSync(tempFilePath);
+	})
+}
 );
