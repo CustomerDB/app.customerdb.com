16b33efb50f329e4f24bbfefb3cdf90eed9e1435
Niklas Quarfot Nielsen <nik@qni.dk>
2020-07-13 18:48:38 -0700

    Invite email job

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const sgMail = require('@sendgrid/mail');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp()
@@ -6,7 +7,82 @@ const os = require('os');
 const fs = require('fs');
 const csv = require('csv-parser');
 
-exports.parseCSV = function(db, dataset, tempFilePath) {
+functions.pubsub.schedule('every 10 minutes').onRun((context) => {
+	let db = admin.firestore();
+
+	db.collection('organizations').get().then(snapshot => {
+		let organizationPromises = [];
+		console.log("Snapshot size", snapshot.size)
+		snapshot.forEach((doc) => {
+			let orgID = doc.id;
+
+
+			organizationPromises.push(new Promise((accept, reject) => {
+
+				let memberPromises = [];
+
+				// list member collections to get.
+				memberPromises.push(db.collection('organizations')
+					.doc(orgID)
+					.collection('members')
+					.where("invited", "==", true)
+					.where("active", "==", false)
+					.where("inviteSentTimestamp", "==", "").get()
+					.then((snapshot) => {
+						let mailPromises = [];
+
+						snapshot.forEach(doc => {
+							mailPromises.push(new Promise((accept, reject) => {
+								let member = doc.data();
+
+								console.log("Sending email to ", member);
+
+								let mailHtml = fs.readFileSync('mail_templates/invite_mail.html', 'utf8');
+								let mailTxt = fs.readFileSync('mail_templates/invite_mail.txt', 'utf8');
+
+								mailHtml = mailHtml.replace("{{orgID}}", orgID);
+								mailTxt = mailTxt.replace("{{orgID}}", orgID);
+
+								sgMail.setApiKey("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");
+								const msg = {
+									to: member.email,
+									from: 'noreply@quantap.com',
+									subject: 'You have been invited to join a team on CustomerDB',
+									text: mailTxt,
+									html: mailHtml
+								};
+								sgMail.send(msg);
+
+								doc.ref.set({
+									inviteSentTimestamp: admin.firestore.FieldValue.serverTimestamp()
+								}, { merge: true }).then(() => {
+									// accept
+									accept();
+								}).catch(e => {
+									reject(e);
+								})
+							}))
+						});
+
+						console.log("Waiting for ", mailPromises.length, " mail promises");
+						return Promise.all(mailPromises).then(() => accept()).catch(e => { reject(e) });
+					}));
+
+				console.log("Waiting for ", memberPromises.length, " member promises");
+				return Promise.all(memberPromises).then(() => accept()).catch(e => { reject(e) });
+			}))
+		});
+
+		console.log("Waiting for ", organizationPromises.length, " organization promises");
+		return Promise.all(organizationPromises);
+	}).then(() => {
+		res.send("OK");
+	}).catch(e => {
+		console.log(e);
+	})
+});
+
+exports.parseCSV = function (db, dataset, tempFilePath) {
 	let highlights = dataset.collection('highlights');
 
 	// Read the CSV.
@@ -18,52 +94,52 @@ exports.parseCSV = function(db, dataset, tempFilePath) {
 	let tags = {};
 
 	return new Promise(function (resolve, reject) {
-			console.log("Read csv ", tempFilePath)
+		console.log("Read csv ", tempFilePath)
 
-			fs.createReadStream(tempFilePath)
-				.pipe(csv())
-				.on('data', function(row) {
-					// Build a list of tags.
-					// TODO: Make a required list of properties.
-					if (!row.hasOwnProperty('Tag')) {
-						console.error(new Error("Row missing 'Tag' field"))
-						return;
-					}
+		fs.createReadStream(tempFilePath)
+			.pipe(csv())
+			.on('data', function (row) {
+				// Build a list of tags.
+				// TODO: Make a required list of properties.
+				if (!row.hasOwnProperty('Tag')) {
+					console.error(new Error("Row missing 'Tag' field"))
+					return;
+				}
 
-					// Empthy columns are not allowed in firestore.
-					if (row.hasOwnProperty('')) {
-						delete row[''];
-					}
+				// Empthy columns are not allowed in firestore.
+				if (row.hasOwnProperty('')) {
+					delete row[''];
+				}
 
-					let highlightRef = highlights.doc();
-					batch.set(highlightRef, row);
-					count++;
+				let highlightRef = highlights.doc();
+				batch.set(highlightRef, row);
+				count++;
 
-					let tag = row['Tag']
-					if (!tags.hasOwnProperty(tag)) {
-						tags[tag] = 0;
-					}
-					tags[tag]++;
+				let tag = row['Tag']
+				if (!tags.hasOwnProperty(tag)) {
+					tags[tag] = 0;
+				}
+				tags[tag]++;
 
-					if (count == batchSize) {
-						console.log(`Batch commit ${batchNum}`);
-						batchPromises.push(batch.commit());
-						batch = db.batch();
-						count = 0;
-						batchNum++;
-					}
-				})
-				.on('end', () => {
+				if (count == batchSize) {
+					console.log(`Batch commit ${batchNum}`);
 					batchPromises.push(batch.commit());
-					resolve(Promise.all(batchPromises));
-				});
-	}).then(function() {
+					batch = db.batch();
+					count = 0;
+					batchNum++;
+				}
+			})
+			.on('end', () => {
+				batchPromises.push(batch.commit());
+				resolve(Promise.all(batchPromises));
+			});
+	}).then(function () {
 		console.log("Recording processed timestamp");
 		let now = new Date();
 		return dataset.set({
 			tags: Object.keys(tags),
 			processedAt: now.toISOString()
-		}, {merge: true});
+		}, { merge: true });
 	});
 };
 
@@ -85,10 +161,10 @@ exports.processCSVUpload = functions.storage.object().onFinalize((object) => {
 
 	let dataset = db.collection('datasets').doc(datasetID);
 
-	let download = bucket.file(filePath).download({destination: tempFilePath});
+	let download = bucket.file(filePath).download({ destination: tempFilePath });
 	return download
 		.then(() => { return exports.parseCSV(db, dataset, tempFilePath) })
-		.then(function() {
+		.then(function () {
 			console.log("Deleting temporary file");
 			return fs.unlinkSync(tempFilePath);
 		});
@@ -96,16 +172,16 @@ exports.processCSVUpload = functions.storage.object().onFinalize((object) => {
 );
 
 exports.deleteDataset = functions.firestore
-    .document('datasets/{datasetID}')
-    .onUpdate((snap, context) => {
+	.document('datasets/{datasetID}')
+	.onUpdate((snap, context) => {
 		let storage = admin.storage();
 		let bucket = storage.bucket();
 		let data = snap.after.data();
 
 		if (data.hasOwnProperty('deletedAt') && data.hasOwnProperty('googleStoragePath')) {
 			console.log("Deleting: " + data);
-			return bucket.file(data.googleStoragePath).delete().then(function() {
+			return bucket.file(data.googleStoragePath).delete().then(function () {
 				return snap.after.ref.delete();
 			})
 		}
-});
+	});
