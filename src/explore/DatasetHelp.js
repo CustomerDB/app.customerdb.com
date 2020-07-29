affab61f90ac0adc0aab37af3c91b8bd528c2124
Connor Doyle <connor@quantap.com>
2020-07-29 00:21:15 -0700

    Add help text and graphics for expore tab.

---
+++ src/explore/DatasetHelp.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import Content from "../shell/Content.js";
+
+import exploreGraphic from "../assets/images/explore.svg";
+
+export default function DatasetHelp(props) {
+  return (
+    <Content>
+      <div className="d-flex flex-column w-100 h-100 justify-content-center align-self-center">
+        <div
+          className="d-flex flex-column justify-content-center align-self-center"
+          style={{ padding: "1rem" }}
+        >
+          <div className="d-flex justify-content-center align-self-center">
+            <div
+              style={{ width: "30rem" }}
+              className="d-flex justify-content-center align-self-center"
+            >
+              <img
+                style={{ width: "100%" }}
+                src={exploreGraphic}
+                alt="Eplore illustration"
+              />
+            </div>
+            <div
+              style={{ maxWidth: "25rem" }}
+              className="justify-content-center align-self-center"
+            >
+              <h4>Explore your customer data</h4>
+              <br />
+              <h5>
+                Collaborate with your team to find patterns in verbatims from
+                multiple customers.
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Content>
+  );
+}
