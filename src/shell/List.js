45cf77785b2843e13b1fd1f169a86e87ec04c088
Niklas Quarfot Nielsen <nik@qni.dk>
2020-07-29 11:54:42 -0700

    Logs for search results

--- src/shell/List.js
+++ src/shell/List.js
@@ -42,15 +42,16 @@ export default class List extends React.Component {
       return <Loading />;
     }
 
-    const CustomHits = connectHits((result) =>
-      result.hits.map((hit) => (
+    const CustomHits = connectHits((result) => {
+      console.log("Recieved search results ", result.hits.length);
+      return result.hits.map((hit) => (
         <List.Item
           key={hit.objectID}
           name={hit.name}
           path={props.path(hit.objectID)}
         />
-      ))
-    );
+      ));
+    });
 
     if (!props.children) {
       return <></>;
