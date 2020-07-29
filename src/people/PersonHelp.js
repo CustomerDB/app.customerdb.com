5fbe73cb72dbaad84b21bdc36dc12c1a48dc3a7c
Niklas Quarfot Nielsen <nik@qni.dk>
2020-07-28 17:22:15 -0700

    Vertical and horizontal alignment for help text

--- src/people/PersonHelp.js
+++ src/people/PersonHelp.js
@@ -11,29 +11,29 @@ import personGraphic from "../assets/images/person.svg";
 export default function PersonHelp(props) {
   return (
     <Content>
-      <div className="roundedBorders" style={{ padding: "1rem" }}>
-        <div className="d-flex">
-          <div>
-            <h4>Organize your customer contacts</h4>
-            <p>
-              This is the space to organize your customer contacts. This helps
-              us connect data about your customers (location, industry, company
-              size etc) with what they tell you. In this way, you can know which
-              customers wants what, who have certain problems and feels which
-              way.
-            </p>
-
-            <p>
-              The more information you add here, the more insights you can get
-              during your analysis.
-            </p>
-          </div>
-          <div style={{ width: "30rem" }}>
-            <img
-              style={{ width: "100%" }}
-              src={personGraphic}
-              alt="Person illustration"
-            />
+      <div className="d-flex flex-column w-100 h-100 justify-content-center align-self-center">
+        <div
+          className="d-flex flex-column justify-content-center align-self-center"
+          style={{ padding: "1rem", width: "60rem" }}
+        >
+          <div className="d-flex justify-content-center align-self-center">
+            <div
+              style={{ width: "30rem" }}
+              className="d-flex justify-content-center align-self-center"
+            >
+              <img
+                style={{ width: "100%" }}
+                src={personGraphic}
+                alt="Person illustration"
+              />
+            </div>
+            <div>
+              <h4>Organize your customer contacts</h4>
+              <h5>
+                The more information you add here, the more insights you can get
+                during your analysis.
+              </h5>
+            </div>
           </div>
         </div>
       </div>
