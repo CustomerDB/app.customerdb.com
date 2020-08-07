b9fa9bdb5abfbd0794deb2f2ef929884441b034d
Niklas Quarfot Nielsen <nik@qni.dk>
2020-08-07 15:23:44 -0700

    WIP

--- src/organization/Organization.js
+++ src/organization/Organization.js
@@ -10,26 +10,108 @@ import { useNavigate, useLocation, useParams } from "react-router-dom";
 
 import { Loading } from "../util/Utils.js";
 
-import {
-  DoorOpen,
-  People,
-  ChatLeftQuote,
-  GearWide,
-  Intersect,
-} from "react-bootstrap-icons";
-
 import OrganizationRoutes from "./OrganizationRoutes.js";
-import Shell from "../shell/Shell.js";
-import Navigation from "../shell/Navigation.js";
 
-export default function Organization(props) {
+import clsx from "clsx";
+import {
+  makeStyles,
+  useTheme,
+  ThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+// Drawer icons
+import GroupIcon from "@material-ui/icons/Group";
+import RecordVoiceOverIcon from "@material-ui/icons/RecordVoiceOver";
+import MultilineChartIcon from "@material-ui/icons/MultilineChart";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    height: "calc(100vh - 64px)",
+  },
+}));
+
+export default function Organization() {
   const { oauthUser, oauthClaims } = useContext(UserAuthContext);
   const { orgID } = useParams();
+
   const navigate = useNavigate();
+  const location = useLocation();
   const [authorized, setAuthorized] = useState(false);
   const [intercomInit, setIntercomInit] = useState(false);
-
-  const location = useLocation();
+  const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
     if (!oauthUser) {
@@ -66,48 +148,138 @@ export default function Organization(props) {
     window.Intercom("update");
   }, [intercomInit, location]);
 
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   if (!authorized) return <Loading />;
 
-  return (
-    <Shell>
-      <Navigation>
-        <Navigation.Top>
-          <Navigation.Item
-            name="People"
-            icon={<People />}
-            path={`/orgs/${orgID}/people`}
-            end={false}
-          />
-          <Navigation.Item
-            name="Data"
-            icon={<ChatLeftQuote />}
-            path={`/orgs/${orgID}/data`}
-            end={false}
-          />
-          <Navigation.Item
-            name="Analyze"
-            icon={<Intersect />}
-            path={`/orgs/${orgID}/analyze`}
-            end={false}
-          />
-        </Navigation.Top>
-        <Navigation.Bottom>
-          <Navigation.Item
-            name="Settings"
-            icon={<GearWide />}
-            path={`/orgs/${orgID}/settings`}
-            end={false}
-          />
-          <Navigation.Item
-            name="Logout"
-            icon={<DoorOpen />}
-            path={`/logout`}
-            end={true}
-          />
-        </Navigation.Bottom>
-      </Navigation>
+  //   let customTheme = {};
+  // Object.assign({}, theme);
+  //   customTheme.palette.primary1Color =
+  //   customTheme.palette.accent1Color = ;
 
-      <OrganizationRoutes />
-    </Shell>
+  const customTheme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#424242",
+      },
+      secondary: {
+        main: "#ff5252",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={customTheme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              People
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <ListItem
+              button
+              key="Customers"
+              selected={location.startsWith(`/orgs/${orgID}/people`)}
+              onClick={() => navigate(`/orgs/${orgID}/people`)}
+            >
+              <ListItemIcon>
+                <GroupIcon />
+              </ListItemIcon>
+              <ListItemText primary="Customers" />
+            </ListItem>
+          </List>
+          <List>
+            <ListItem
+              button
+              key="Data"
+              onClick={() => navigate(`/orgs/${orgID}/data`)}
+            >
+              <ListItemIcon>
+                <RecordVoiceOverIcon />
+              </ListItemIcon>
+              <ListItemText primary="Data" />
+            </ListItem>
+          </List>
+          <List>
+            <ListItem
+              button
+              key="Analysis"
+              onClick={() => navigate(`/orgs/${orgID}/analyze`)}
+            >
+              <ListItemIcon>
+                <MultilineChartIcon />
+              </ListItemIcon>
+              <ListItemText primary="Analysis" />
+            </ListItem>
+          </List>
+
+          <Divider />
+          {/* <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List> */}
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <OrganizationRoutes />
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
