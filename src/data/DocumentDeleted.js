cf5e13a500535fa7cc32a794b66bd9384d3ec275
Niklas Quarfot Nielsen <nik@qni.dk>
2020-08-07 10:18:02 -0700

    Make deleted docs not crash the frontend

--- src/data/DocumentDeleted.js
+++ src/data/DocumentDeleted.js
@@ -4,21 +4,23 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+import Moment from "react-moment";
+
 export default function DocumentDeleted(props) {
-  let date = this.state.document.deletionTimestamp.toDate();
+  let date = props.document.deletionTimestamp.toDate();
 
   return (
     <Container>
       <Row noGutters={true}>
         <Col>
-          <h3>{this.state.document.name}</h3>
+          <h3>{props.document.name}</h3>
         </Col>
       </Row>
       <Row noGutters={true}>
         <Col>
           <p>
-            This document was deleted at {date.toString()} by{" "}
-            {this.state.document.deletedBy}
+            This document was deleted <Moment fromNow date={date} /> by{" "}
+            {props.document.deletedBy}
           </p>
         </Col>
       </Row>
