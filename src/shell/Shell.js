0a261bd175a98355e83bb5ef5188d727b92ac23e
Niklas Quarfot Nielsen <nik@qni.dk>
2020-08-20 11:51:49 -0700

    Hide nav bar on sm

--- src/shell/Shell.js
+++ src/shell/Shell.js
@@ -76,7 +76,7 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: "hidden",
-    width: theme.spacing(7) + 1,
+    width: 0,
     [theme.breakpoints.up("sm")]: {
       width: theme.spacing(9) + 1,
     },
