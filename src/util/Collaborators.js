bbc3b94d6893df06e58d1d3afd1c487ace35a824
Niklas Quarfot Nielsen <nik@qni.dk>
2020-08-20 22:03:47 -0700

    Tooltip and robust to missing oauth user details

--- src/util/Collaborators.js
+++ src/util/Collaborators.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 
 import Avatar from "@material-ui/core/Avatar";
 import AvatarGroup from "@material-ui/lab/AvatarGroup";
+import Tooltip from "@material-ui/core/Tooltip";
 import UserAuthContext from "../auth/UserAuthContext.js";
 
 const syncPeriod = 5000;
@@ -15,33 +16,43 @@ export default function Collaborators(props) {
     let collaboratorRef = props.dbRef
       .collection("collaborators")
       .doc(oauthUser.email);
-    collaboratorRef
-      .set({
-        email: oauthUser.email,
-        name: oauthUser.displayName,
-        photoURL: oauthUser.photoURL,
-        expires: window.firebase.firestore.Timestamp.now().toMillis() + 5000,
-      })
-      .then(() => {
-        props.dbRef
-          .collection("collaborators")
-          .get()
-          .then((snapshot) => {
-            let newCollaborators = [];
-            snapshot.forEach((doc) => {
-              let now = window.firebase.firestore.Timestamp.now().toMillis();
-              let collaborator = doc.data();
-              if (collaborator.expires < now) {
-                doc.ref.delete();
-                return;
-              }
 
-              newCollaborators.push(doc.data());
-            });
+    let collaborator = {
+      expires: window.firebase.firestore.Timestamp.now().toMillis() + 5000,
+    };
 
-            setCollaborators(newCollaborators);
+    if (oauthUser.email) {
+      collaborator.email = oauthUser.email;
+    }
+
+    if (oauthUser.displayName) {
+      collaborator.name = oauthUser.displayName;
+    }
+
+    if (oauthUser.photoURL) {
+      collaborator.photoURL = oauthUser.photoURL;
+    }
+
+    collaboratorRef.set(collaborator).then(() => {
+      props.dbRef
+        .collection("collaborators")
+        .get()
+        .then((snapshot) => {
+          let newCollaborators = [];
+          let now = window.firebase.firestore.Timestamp.now().toMillis();
+          snapshot.forEach((doc) => {
+            let collaborator = doc.data();
+            if (collaborator.expires < now) {
+              doc.ref.delete();
+              return;
+            }
+
+            newCollaborators.push(doc.data());
           });
-      });
+
+          setCollaborators(newCollaborators);
+        });
+    });
   };
 
   useEffect(() => {
@@ -67,11 +78,13 @@ export default function Collaborators(props) {
   return (
     <AvatarGroup max={4}>
       {collaborators.map((collaborator) => (
-        <Avatar
-          key={collaborator.email}
-          alt={collaborator.name}
-          src={collaborator.photoURL}
-        />
+        <Tooltip title={collaborator.name}>
+          <Avatar
+            key={collaborator.email}
+            alt={collaborator.name}
+            src={collaborator.photoURL}
+          />
+        </Tooltip>
       ))}
     </AvatarGroup>
   );
