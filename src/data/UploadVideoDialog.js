55fcf5d75d4c5a10e06f6ecac017fd0a2f719e1e
Niklas Quarfot Nielsen <nik@qni.dk>
2020-08-24 15:33:23 -0700

    WIP

--- src/data/UploadVideoDialog.js
+++ src/data/UploadVideoDialog.js
@@ -6,9 +6,12 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
 
 export default function UploadVideoDialog({ open, setOpen }) {
-  const [files, setFiles] = useState();
+  const [file, setFile] = useState();
+  const [name, setName] = useState();
 
   const cancel = () => {
     setOpen(false);
@@ -16,11 +19,13 @@ export default function UploadVideoDialog({ open, setOpen }) {
 
   console.log("Upload video dialog", open);
 
-  const onDrop = (files) => {
-    setFiles(files);
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
   };
 
-  console.log(files);
+  console.log(file);
+
+  const onSave = () => {};
 
   return (
     <Dialog
@@ -28,33 +33,52 @@ export default function UploadVideoDialog({ open, setOpen }) {
       onClose={cancel}
       aria-labelledby="upload-dialog-title"
       aria-describedby="upload-dialog-description"
+      fullWidth={true}
+      maxWidth="md"
     >
       <DialogTitle id="upload-dialog-title">
         Upload video for transcription
       </DialogTitle>
       <DialogContent>
-        <input
-          accept="video/*,audio/*"
-          style={{ display: "none" }}
-          id="raised-button-file"
-          multiple
-          type="file"
-          onChange={(e) => {
-            console.log(e.target);
-          }}
-        />
-        <label htmlFor="raised-button-file">
-          <Button variant="raised" component="span">
-            Select video or audio file
-          </Button>
-        </label>
+        <Grid container spacing={3}>
+          <Grid container item>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                id="name"
+                label="Name"
+                fullWidth
+                value={name}
+                margin="dense"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <input
+                accept="video/*,audio/*"
+                style={{ display: "none" }}
+                id="raised-button-file"
+                multiple
+                type="file"
+                onChange={onChange}
+              />
+              <label htmlFor="raised-button-file">
+                <Button variant="raised" component="span">
+                  {file ? file.name : "Select video or audio file"}
+                </Button>
+              </label>
+            </Grid>
+          </Grid>
+        </Grid>
       </DialogContent>
       <DialogActions>
         <Button onClick={cancel} color="primary">
           Cancel
         </Button>
         <Button disabled color="primary">
-          Create
+          Upload
         </Button>
       </DialogActions>
     </Dialog>
