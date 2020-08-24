918f432a1d1a50027ed00e9efda8a55606939828
Niklas Quarfot Nielsen <nik@qni.dk>
2020-08-24 16:31:27 -0700

    Snapshot

--- src/data/UploadVideoDialog.js
+++ src/data/UploadVideoDialog.js
@@ -1,19 +1,62 @@
 import React, { useState } from "react";
 
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import FormControl from "@material-ui/core/FormControl";
 import Grid from "@material-ui/core/Grid";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
+import clsx from "clsx";
+import { green } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    "&:hover": {
+      backgroundColor: green[700],
+    },
+  },
+  buttonProgress: {
+    color: green[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
 
 export default function UploadVideoDialog({ open, setOpen }) {
+  const classes = useStyles();
+
+  const [uploading, setUploading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+
   const [file, setFile] = useState();
   const [name, setName] = useState();
+  const [speakers, setSpeakers] = useState(1);
 
   const cancel = () => {
+    setFile();
+    setName();
+    setSpeakers(1);
+    setUploading(false);
     setOpen(false);
   };
 
@@ -23,9 +66,21 @@ export default function UploadVideoDialog({ open, setOpen }) {
     setFile(e.target.files[0]);
   };
 
+  const handleSpeakerChange = (e) => {
+    setSpeakers(e.target.value);
+  };
+
   console.log(file);
 
-  const onSave = () => {};
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success,
+  });
+
+  const startUpload = () => {
+    setUploading(true);
+
+    // Store name, speaker count and path in operation document.
+  };
 
   return (
     <Dialog
@@ -40,7 +95,7 @@ export default function UploadVideoDialog({ open, setOpen }) {
         Upload video for transcription
       </DialogTitle>
       <DialogContent>
-        <Grid container spacing={3}>
+        <Grid container spacing={2}>
           <Grid container item>
             <Grid item xs={12}>
               <TextField
@@ -55,6 +110,27 @@ export default function UploadVideoDialog({ open, setOpen }) {
                 }}
               />
             </Grid>
+          </Grid>
+          <Grid container item>
+            <Grid item xs={12}>
+              <FormControl variant="outlined" style={{ width: "10rem" }}>
+                <InputLabel id="speaker-label">Speakers</InputLabel>
+                <Select
+                  labelId="speaker-label"
+                  id="speaker"
+                  value={speakers}
+                  onChange={handleSpeakerChange}
+                  label="speakers"
+                >
+                  <MenuItem value={1}>1</MenuItem>
+                  <MenuItem value={2}>2</MenuItem>
+                  <MenuItem value={3}>3</MenuItem>
+                  <MenuItem value={4}>4</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Grid container item>
             <Grid item xs={12}>
               <input
                 accept="video/*,audio/*"
@@ -77,9 +153,21 @@ export default function UploadVideoDialog({ open, setOpen }) {
         <Button onClick={cancel} color="primary">
           Cancel
         </Button>
-        <Button disabled color="primary">
-          Upload
-        </Button>
+
+        <div className={classes.wrapper}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={buttonClassname}
+            disabled={uploading || !file || !name || !speakers}
+            onClick={startUpload}
+          >
+            Upload
+          </Button>
+          {uploading && (
+            <CircularProgress size={24} className={classes.buttonProgress} />
+          )}
+        </div>
       </DialogActions>
     </Dialog>
   );
