3ca5a6fb09e4b4063bf1dc266fe75c6c079ca678
Connor Doyle <connor@quantap.com>
2020-09-30 14:44:16 -0700

    Implement patch-based reconciliation

--- src/editor/CollabEditor.js
+++ src/editor/CollabEditor.js
@@ -56,6 +56,8 @@ export default function CollabEditor({
   const [revisionDelta, setRevisionDelta] = useState();
   const [revisionTimestamp, setRevisionTimestamp] = useState();
 
+  const uncommittedDeltas = useRef([]);
+
   const { oauthClaims } = useContext(UserAuthContext);
 
   const localDelta = useRef(new Delta([]));
@@ -107,7 +109,7 @@ export default function CollabEditor({
     }
 
     console.debug(
-      "Subscribing to deltas since",
+      "Subscribing to remote deltas since",
       latestDeltaTimestamp.current.toDate()
     );
 
@@ -115,84 +117,71 @@ export default function CollabEditor({
       .orderBy("timestamp", "asc")
       .where("timestamp", ">", revisionTimestamp)
       .onSnapshot((snapshot) => {
-        let newDeltas = [];
-        snapshot.forEach((delta) => {
-          let data = delta.data();
+        // filter out newly committed deltas from uncommittedDeltas
+        let oldUncommittedDeltas = uncommittedDeltas.current;
+        let newUncommittedDeltas = uncommittedDeltas.current;
+        snapshot.docs.forEach((deltaDoc) => {
+          newUncommittedDeltas = newUncommittedDeltas.filter(
+            (d) => d.ID !== deltaDoc.id
+          );
+        });
+        uncommittedDeltas.current = newUncommittedDeltas;
 
-          // Skip deltas with no timestamp
-          if (data.timestamp === null) {
-            // console.debug("skipping delta with no timestamp");
-            return;
-          }
-
-          // Skip deltas older than the latest timestamp we have applied already
-          let haveSeenBefore =
-            data.timestamp.valueOf() <= latestDeltaTimestamp.current.valueOf();
-
-          if (haveSeenBefore) {
-            // console.debug("Dropping delta with timestamp ", data.timestamp);
-            return;
-          }
-
-          let newDelta = new Delta(data.ops);
-
-          // Hang the editorID off of the delta.
-          newDelta.editorID = data.editorID;
-
-          // Skip deltas from this client
-          if (data.editorID === editorID) {
-            // console.debug("skipping delta from this client");
-            return;
-          }
-
-          newDeltas.push(newDelta);
-          latestDeltaTimestamp.current = data.timestamp;
+        let committedDeltas = [];
+        snapshot.forEach((deltaDoc) => {
+          committedDeltas.push(new Delta(deltaDoc.data().ops));
         });
 
-        if (newDeltas.length === 0) {
-          // console.debug("no new deltas to apply");
-          return;
-        }
-
-        console.debug(
-          "applying deltas to editor",
-          JSON.stringify(newDeltas.flatMap((d) => d.ops))
-        );
-
-        // What we have:
-        // - localDelta: the buffered local edits that haven't been uploaded yet
-        // - editor.getContents(): document delta representing local editor content
-
-        let selection = editor.getSelection();
-        let selectionIndex = selection ? selection.index : 0;
-
-        // Compute inverse of local delta.
         let editorContents = editor.getContents();
-        console.debug("editorContents", editorContents);
 
-        console.debug("localDelta (before)", localDelta.current);
+        console.debug("localDelta.current", localDelta.current);
         let inverseLocalDelta = localDelta.current.invert(editorContents);
         console.debug("inverseLocalDelta", inverseLocalDelta);
 
-        // Undo local edits
+        // Compute local: latest revision + old committed deltas + old uncommitted deltas
+        //                == editor content - local delta
+        let local = editorContents.compose(inverseLocalDelta);
+        console.debug("local", local);
+
+        // Compute remote: latest revision + new committed deltas + new uncommitted deltas
+        let remote = revisionDelta;
+        committedDeltas.forEach((delta) => {
+          remote = remote.compose(delta);
+        });
+        newUncommittedDeltas.forEach((delta) => {
+          remote = remote.compose(delta);
+        });
+        console.debug("remote", remote);
+
+        // Compute update patch from local to remote
+        let diff = local.diff(remote);
+        console.debug("diff", diff);
+
+        if (diff.ops.length === 0) {
+          console.debug("diff is empty; nothing to do");
+          return;
+        }
+
+        // Unapply the local delta from the editor
+        let selection = editor.getSelection();
+        let selectionIndex = selection ? selection.index : 0;
+
         selectionIndex = inverseLocalDelta.transformPosition(selectionIndex);
-        console.debug("unapplying local delta");
+        console.debug("unapplying local delta", inverseLocalDelta);
         editor.updateContents(inverseLocalDelta);
 
-        const serverFirst = true;
-        newDeltas.forEach((delta) => {
-          editor.updateContents(delta);
-          selectionIndex = delta.transformPosition(selectionIndex);
-          localDelta.current = delta.transform(localDelta.current, serverFirst);
-        });
+        // Apply the update patch to the editor
+        console.debug("applying remote update patch", diff);
+        selectionIndex = diff.transformPosition(selectionIndex);
+        editor.updateContents(diff);
 
-        // Reapply local edits
-        console.debug(
-          "re-applying transformed local delta",
-          localDelta.current
-        );
-        editor.updateContents(localDelta.current);
+        // Re-apply the transformed local delta to the editor
+
+        // Transform the local delta buffer by diff
+        localDelta.current = diff.transform(localDelta.current);
         selectionIndex = localDelta.current.transformPosition(selectionIndex);
+        console.debug("re-applying transformed local delta");
+        editor.updateContents(localDelta.current);
 
         if (selection) {
           console.debug("updating selection index");
@@ -200,6 +189,90 @@ export default function CollabEditor({
         }
       });
   }, [editorID, revisionTimestamp, deltasRef, quillRef]);
+
+  //  return deltasRef
+  //    .orderBy("timestamp", "asc")
+  //    .where("timestamp", ">", revisionTimestamp)
+  //    .onSnapshot((snapshot) => {
+  //      let newDeltas = [];
+  //      snapshot.forEach((delta) => {
+  //        let data = delta.data();
+
+  //        // Skip deltas older than the latest timestamp we have applied already
+  //        let haveSeenBefore =
+  //          data.timestamp.valueOf() <= latestDeltaTimestamp.current.valueOf();
+
+  //        if (haveSeenBefore) {
+  //          // console.debug("Dropping delta with timestamp ", data.timestamp);
+  //          return;
+  //        }
+
+  //        let newDelta = new Delta(data.ops);
+
+  //        // Hang the editorID off of the delta.
+  //        newDelta.editorID = data.editorID;
+
+  //        // Skip deltas from this client
+  //        if (data.editorID === editorID) {
+  //          // console.debug("skipping delta from this client");
+  //          return;
+  //        }
+
+  //        newDeltas.push(newDelta);
+  //        latestDeltaTimestamp.current = data.timestamp;
+  //      });
+
+  //      if (newDeltas.length === 0) {
+  //        // console.debug("no new deltas to apply");
+  //        return;
+  //      }
+
+  //      console.debug(
+  //        "applying deltas to editor",
+  //        JSON.stringify(newDeltas.flatMap((d) => d.ops))
+  //      );
+
+  //      // What we have:
+  //      // - localDelta: the buffered local edits that haven't been uploaded yet
+  //      // - editor.getContents(): document delta representing local editor content
+
+  //      let selection = editor.getSelection();
+  //      let selectionIndex = selection ? selection.index : 0;
+
+  //      // Compute inverse of local delta.
+  //      let editorContents = editor.getContents();
+  //      console.debug("editorContents", editorContents);
+
+  //      console.debug("localDelta (before)", localDelta.current);
+  //      let inverseLocalDelta = localDelta.current.invert(editorContents);
+  //      console.debug("inverseLocalDelta", inverseLocalDelta);
+
+  //      // Undo local edits
+  //      selectionIndex = inverseLocalDelta.transformPosition(selectionIndex);
+  //      console.debug("unapplying local delta");
+  //      editor.updateContents(inverseLocalDelta);
+
+  //      const serverFirst = true;
+  //      newDeltas.forEach((delta) => {
+  //        editor.updateContents(delta);
+  //        selectionIndex = delta.transformPosition(selectionIndex);
+  //        localDelta.current = delta.transform(localDelta.current, serverFirst);
+  //      });
+
+  //      // Reapply local edits
+  //      console.debug(
+  //        "re-applying transformed local delta",
+  //        localDelta.current
+  //      );
+  //      editor.updateContents(localDelta.current);
+  //      selectionIndex = localDelta.current.transformPosition(selectionIndex);
+
+  //      if (selection) {
+  //        console.debug("updating selection index");
+  //        editor.setSelection(selectionIndex, selection.length);
+  //      }
+  //    });
+  //}, [editorID, revisionTimestamp, deltasRef, quillRef]);
 
   // Register timers to periodically sync local changes with firestore.
   useEffect(() => {
@@ -218,7 +291,10 @@ export default function CollabEditor({
       let ops = localDelta.current.ops.slice(0, opsIndex);
       localDelta.current = new Delta(localDelta.current.ops.slice(opsIndex));
 
+      let deltaID = uuidv4();
+
       let deltaDoc = {
+        ID: deltaID,
         editorID: editorID,
         userEmail: oauthClaims.email,
         timestamp: firebaseClient.firestore.FieldValue.serverTimestamp(),
@@ -226,7 +302,8 @@ export default function CollabEditor({
       };
 
       console.debug("uploading delta", deltaDoc);
-      deltasRef.doc().set(deltaDoc);
+      uncommittedDeltas.current.push(deltaDoc);
+      deltasRef.doc(deltaID).set(deltaDoc);
     };
 
     console.debug(`starting periodic syncDeltas every ${deltaSyncPeriod}ms`);
