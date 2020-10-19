92039ecd637ad4beb9d00cb36c2e15a254acde88
Connor Doyle <connor@quantap.com>
2020-10-19 15:51:14 -0700

    Removed unused import from JoinOrg

--- src/auth/JoinOrg.js
+++ src/auth/JoinOrg.js
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import React, { useContext, useEffect, useState } from "react";
 import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
 
