d1a4ada39b5e9691078ca49aa0f5c8d803680b77
Niklas Quarfot Nielsen <nik@qni.dk>
2020-10-22 17:25:54 -0700

    Guest preflight tokens

--- functions/twilio.js
+++ functions/twilio.js
@@ -8,6 +8,25 @@ const twilioAccountSid = functions.config().twilio.account_sid;
 const twilioApiKeySID = functions.config().twilio.api_key_sid;
 const twilioApiKeySecret = functions.config().twilio.api_key_secret;
 
+function getCallForGuest(callID, token) {
+  let db = admin.firestore();
+  let callRef = db.collectionGroup("calls").where("ID", "==", callID).limit(1);
+
+  return callRef.get().then((snapshot) => {
+    if (snapshot.docs.length == 0) {
+      throw Error(`Call ${callID} doesn't exist`);
+    }
+
+    let call = snapshot.docs[0].data();
+
+    if (call.token !== token) {
+      throw Error(`Incorrect token for call ${data.callID}`);
+    }
+
+    return call;
+  });
+}
+
 exports.getGuestAccessToken = functions.https.onCall((data, context) => {
   if (!data.callID) {
     throw Error("callID required");
@@ -21,24 +40,7 @@ exports.getGuestAccessToken = functions.https.onCall((data, context) => {
     throw Error("name required");
   }
 
-  let db = admin.firestore();
-
-  let callRef = db
-    .collectionGroup("calls")
-    .where("ID", "==", data.callID)
-    .limit(1);
-
-  return callRef.get().then((snapshot) => {
-    if (snapshot.docs.length == 0) {
-      throw Error(`Call ${data.callID} doesn't exist`);
-    }
-
-    let call = snapshot.docs[0].data();
-
-    if (call.token !== data.token) {
-      throw Error(`Incorrect token for call ${data.callID}`);
-    }
-
+  return getCallForGuest(data.callID, data.token).then(() => {
     const token = new AccessToken(
       twilioAccountSid,
       twilioApiKeySID,
@@ -52,7 +54,7 @@ exports.getGuestAccessToken = functions.https.onCall((data, context) => {
     token.addGrant(videoGrant);
 
     return {
-      token: token.toJwt(),
+      accessToken: token.toJwt(),
     };
   });
 });
@@ -74,7 +76,6 @@ exports.getInterviewAccessToken = functions.https.onCall((data, context) => {
   }
 
   let db = admin.firestore();
-
   let callRef = db
     .collectionGroup("calls")
     .where("ID", "==", data.callID)
@@ -104,50 +105,62 @@ exports.getInterviewAccessToken = functions.https.onCall((data, context) => {
     token.addGrant(videoGrant);
 
     return {
-      token: token.toJwt(),
+      accessToken: token.toJwt(),
     };
   });
 });
 
 exports.getPreflightAccessTokens = functions.https.onCall((data, context) => {
   // Require authenticated requests
-  if (!context.auth || !context.auth.token || !context.auth.token.orgID) {
+
+  // First case is for guest pre-flight tests.
+  // The second case is for logged in users.
+  let authorized = Promise.resolve();
+  if (data.callID && data.token) {
+    authorized = getCallForGuest(data.callID, data.token);
+  } else if (
+    !context.auth ||
+    !context.auth.token ||
+    !context.auth.token.orgID
+  ) {
     throw new functions.https.HttpsError(
       "permission-denied",
       "Authentication required."
     );
   }
 
-  if (!data.room || !data.publisherIdentity || !data.subscriberIdentity) {
-    throw Error("room, publisherIdentity and subscriberIdentity required");
-  }
-
-  const videoGrant = new VideoGrant({ room: data.room });
-
-  const publisherToken = new AccessToken(
-    twilioAccountSid,
-    twilioApiKeySID,
-    twilioApiKeySecret,
-    {
-      ttl: MAX_ALLOWED_SESSION_DURATION,
+  return authorized.then(() => {
+    if (!data.room || !data.publisherIdentity || !data.subscriberIdentity) {
+      throw Error("room, publisherIdentity and subscriberIdentity required");
     }
-  );
-  publisherToken.identity = data.publisherIdentity;
-  publisherToken.addGrant(videoGrant);
 
-  const subscriberToken = new AccessToken(
-    twilioAccountSid,
-    twilioApiKeySID,
-    twilioApiKeySecret,
-    {
-      ttl: MAX_ALLOWED_SESSION_DURATION,
-    }
-  );
-  subscriberToken.identity = data.subscriberIdentity;
-  subscriberToken.addGrant(videoGrant);
+    const videoGrant = new VideoGrant({ room: data.room });
 
-  return {
-    publisherToken: publisherToken.toJwt(),
-    subscriberToken: subscriberToken.toJwt(),
-  };
+    const publisherToken = new AccessToken(
+      twilioAccountSid,
+      twilioApiKeySID,
+      twilioApiKeySecret,
+      {
+        ttl: MAX_ALLOWED_SESSION_DURATION,
+      }
+    );
+    publisherToken.identity = data.publisherIdentity;
+    publisherToken.addGrant(videoGrant);
+
+    const subscriberToken = new AccessToken(
+      twilioAccountSid,
+      twilioApiKeySID,
+      twilioApiKeySecret,
+      {
+        ttl: MAX_ALLOWED_SESSION_DURATION,
+      }
+    );
+    subscriberToken.identity = data.subscriberIdentity;
+    subscriberToken.addGrant(videoGrant);
+
+    return {
+      publisherToken: publisherToken.toJwt(),
+      subscriberToken: subscriberToken.toJwt(),
+    };
+  });
 });
