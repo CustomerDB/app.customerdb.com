6f131cee7ba3a03a457efb3f8c4be9cb9e024f3b
Niklas Quarfot Nielsen <nik@qni.dk>
2020-10-23 23:39:38 -0700

    WIP of functions

--- functions/twilio.js
+++ functions/twilio.js
@@ -2,11 +2,20 @@ const functions = require("firebase-functions");
 const AccessToken = require("twilio").jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
 const admin = require("firebase-admin");
+const tmp = require("tmp");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const request = require("request");
 
 const MAX_ALLOWED_SESSION_DURATION = 14400;
 const twilioAccountSid = functions.config().twilio.account_sid;
 const twilioApiKeySID = functions.config().twilio.api_key_sid;
 const twilioApiKeySecret = functions.config().twilio.api_key_secret;
+const Twilio = require("twilio");
+
+const client = new Twilio(twilioApiKeySID, twilioApiKeySecret, {
+  accountSid: twilioAccountSid,
+});
 
 function getCallForGuest(callID, token) {
   let db = admin.firestore();
@@ -194,3 +203,230 @@ exports.getPreflightAccessTokens = functions.https.onCall((data, context) => {
     };
   });
 });
+
+exports.startComposition = functions.pubsub
+  .schedule("every 2 minutes")
+  .onRun(() => {
+    let db = admin.firestore();
+
+    let callsRef = db
+      .collectionGroup("calls")
+      .where("compositionRequestedTimestamp", "==", "")
+      .orderBy("roomSid");
+
+    callsRef.get().then((snapshot) => {
+      return Promise.all(
+        snapshot.docs.map((doc) => {
+          let call = doc.data();
+          return client.video.recordings
+            .list({
+              groupingSid: [call.roomSid],
+            })
+            .then((recordings) => {
+              let done = true;
+              recordings.forEach((r) => {
+                if (r.status !== "completed") {
+                  done = false;
+                  return;
+                }
+              });
+
+              if (!done) {
+                return;
+              }
+
+              return client.video.compositions
+                .create({
+                  roomSid: call.roomSid,
+                  audioSources: ["*"],
+                  videoLayout: {
+                    transcode: {
+                      video_sources: ["*"],
+                    },
+                  },
+                  statusCallback:
+                    "https://us-central1-customerdb-staging.cloudfunctions.net/twilio-compositionStatus",
+                  format: "mp4",
+                })
+                .then((result) => {
+                  console.log(result);
+                  return doc.ref.update({
+                    compositionRequestedTimestamp: admin.firestore.FieldValue.serverTimestamp(),
+                  });
+                });
+            });
+        })
+      );
+    });
+  });
+
+exports.roomStatus = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).send("Method not allowed");
+    return;
+  }
+
+  let status = req.body;
+
+  if (!status.StatusCallbackEvent || !status.RoomSid || !status.RoomName) {
+    res.status(400).send("Bad request");
+    return;
+  }
+
+  if (status.RoomName.startsWith("preflight")) {
+    res.send("OK");
+    return;
+  }
+
+  if (status.StatusCallbackEvent == "room-ended") {
+    console.log("Starting composition");
+
+    let db = admin.firestore();
+    let callID = status.RoomName;
+
+    db.collectionGroup("calls")
+      .where("ID", "==", callID)
+      .limit(1)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.size == 0) {
+          res.status(404).send("Call not found");
+          return;
+        }
+        let callRef = snapshot.docs[0].ref;
+
+        callRef.update({
+          callEndedTimestamp: admin.firestore.Timestamp.fromDate(
+            new Date(status.Timestamp)
+          ),
+          roomSid: status.RoomSid,
+          compositionRequestedTimestamp: "",
+        });
+
+        res.send("OK");
+      });
+  } else {
+    res.send("OK");
+  }
+});
+
+exports.compositionStatus = functions.https.onRequest(async (req, res) => {
+  if (req.method !== "POST") {
+    res.status(405).send("Method not allowed");
+    return;
+  }
+
+  let status = req.body;
+
+  if (!status.CompositionSid) {
+    res.status(400).send("Bad request");
+    return;
+  }
+
+  let db = admin.firestore();
+  let callsRef = db
+    .collectionGroup("calls")
+    .where("roomSid", "==", status.RoomSid)
+    .limit(1);
+
+  return callsRef.get().then((snapshot) => {
+    if (snapshot.size == 0) {
+      res.status(403).send("Call not found");
+      return;
+    }
+
+    let callRef = snapshot.docs[0].ref;
+    return callRef
+      .update({
+        compositionSid: status.CompositionSid,
+      })
+      .then(() => {
+        res.send("OK");
+      });
+  });
+});
+
+exports.onCompositionReady = functions
+  .runWith({
+    timeoutSeconds: 540,
+    memory: "2GB",
+  })
+  .firestore.document("organizations/{orgID}/calls/{callID}")
+  .onUpdate((change, context) => {
+    let call = change.after.data();
+    if (!call.compositionSid) {
+      console.log("Composition not available");
+      return;
+    }
+
+    const tmpobj = tmp.fileSync();
+
+    const compositionSid = call.compositionSid;
+    const uri =
+      "https://video.twilio.com/v1/Compositions/" +
+      compositionSid +
+      "/Media?Ttl=3600";
+
+    return client
+      .request({
+        method: "GET",
+        uri: uri,
+      })
+      .then(async (response) => {
+        // For example, download the media to a local file
+        let localPath = tmpobj.name + ".mp4";
+        console.log("localPath", localPath);
+        const file = fs.createWriteStream(localPath);
+        console.log("response: ", response);
+        const r = request(response.body.redirect_to);
+        r.on("response", (res) => {
+          res.pipe(file);
+        });
+
+        r.on("end", async () => {
+          console.log(`Video downloaded to ${localPath}`);
+
+          let transcriptionID = uuidv4();
+
+          let outputPath = `${call.organizationID}/transcriptions/${transcriptionID}/input/out.mp4`;
+
+          let db = admin.firestore();
+          let transcriptionRef = db
+            .collection("organizations")
+            .doc(call.organizationID)
+            .collection("transcriptions")
+            .doc(transcriptionID);
+          return transcriptionRef
+            .set({
+              ID: transcriptionID,
+              speakers: 4,
+              createdBy: "",
+              creationTimestamp: admin.firestore.FieldValue.serverTimestamp(),
+              deletionTimestamp: "",
+              inputPath: outputPath,
+              thumbnailToken: "",
+              orgID: call.organizationID,
+              documentID: call.documentID,
+            })
+            .then(() => {
+              let documentRef = db
+                .collection("organizations")
+                .doc(call.organizationID)
+                .collection("documents")
+                .doc(call.documentID);
+              return documentRef.update({
+                pending: true,
+                transcription: transcriptionID,
+              });
+            })
+            .then(() =>
+              admin.storage().bucket().upload(tmpobj.name, {
+                destination: outputPath,
+              })
+            );
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching /Media resource " + error);
+      });
+  });
