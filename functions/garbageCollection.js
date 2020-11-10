c8e1923cf2ec52f7413de1f652613f202712e2a6
Niklas Quarfot Nielsen <nik@qni.dk>
2020-11-09 21:14:14 -0800

    Reduce to people collection to start

--- functions/garbageCollection.js
+++ functions/garbageCollection.js
@@ -2,42 +2,49 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const firebase_tools = require("firebase-tools");
 
-exports.people = functions.pubsub
-  // .schedule("every 1 hours")
-  // .onRun((context) => {
-  .topic("garbageCollection-people")
-  .onPublish((message) => {
-    let db = admin.firestore();
+function garbageCollect(collection) {
+  let db = admin.firestore();
 
-    let now = new Date();
-    const retentionDays = 30;
-    const expiryTime = now.getTime() - retentionDays * 24 * 60 * 60 * 1000;
-    const expiryDate = new Date(expiryTime);
+  let now = new Date();
+  const retentionDays = 30;
+  const expiryTime = now.getTime() - retentionDays * 24 * 60 * 60 * 1000;
+  const expiryDate = new Date(expiryTime);
 
-    return db
-      .collectionGroup("people")
-      .where("deletionTimestamp", "<", expiryDate)
-      .get()
-      .then((peopleSnapshot) =>
-        Promise.all(
-          peopleSnapshot.docs.map((personDoc) => {
-            if (!personDoc.exists) {
-              return;
-            }
+  console.log(`${collection} garbage collection started`);
 
-            let person = personDoc.data();
-            const path = person
-              .toString()
-              .substring(person.root.toString().length);
-            console.log(`Should delete ${path}`);
-            // return firebase_tools.firestore
-            //   .delete(path, {
-            //     project: process.env.GCLOUD_PROJECT,
-            //     recursive: true,
-            //     yes: true,
-            //     token: functions.config().fb.token
-            //   });
-          })
-        )
-      );
+  return db
+    .collectionGroup(collection)
+    .where("deletionTimestamp", "<", expiryDate)
+    .get()
+    .then((snapshot) =>
+      Promise.all(
+        snapshot.docs.map((doc) => {
+          if (!doc.exists) {
+            return;
+          }
+
+          let ref = doc.ref;
+          let path = ref._path.segments.join("/");
+
+          console.warn(`Deleting ${path} recursively...`);
+
+          return firebase_tools.firestore.delete(path, {
+            project: process.env.GCLOUD_PROJECT,
+            recursive: true,
+            yes: true,
+            token: functions.config().system.token,
+          });
+        })
+      )
+    );
+}
+
+exports.people = functions
+  .runWith({
+    timeoutSeconds: 540,
+    memory: "2GB",
+  })
+  .pubsub.schedule("every 1 hours")
+  .onRun((context) => {
+    return garbageCollect("people");
   });
