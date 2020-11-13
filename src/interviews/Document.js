c8120650fd3f66a2cba8d61b924fc21b679da909
Niklas Q Nielsen <nik@qni.dk>
2020-11-12 17:45:03 -0800

    Remove border from editor

--- src/interviews/Document.js
+++ src/interviews/Document.js
@@ -29,11 +29,12 @@ import useFirestore from "../db/Firestore.js";
 
 const useStyles = makeStyles({
   documentPaper: {
-    margin: "1rem 1rem 1rem 2rem",
+    margin: "0 1rem 1rem 2rem",
     padding: "1rem 2rem 4rem 2rem",
     minHeight: "48rem",
     width: "100%",
     maxWidth: "80rem",
+    borderRight: "1px solid rgba(0, 0, 0, 0.12)"
   },
   tabs: {
     width: "100%",
@@ -201,7 +202,7 @@ export default function Document(props) {
   }
 
   return (
-    <Grid container item md={12} lg={9} xl={10} spacing={0}>
+    <Grid container item md={12} lg={9} xl={10} spacing={0} style={{backgroundColor: "white"}}>
       <Grid
         style={{ position: "relative", height: "100%" }}
         container
