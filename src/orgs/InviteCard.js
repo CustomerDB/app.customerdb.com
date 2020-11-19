75ec50369aa0dbde10c84c76239bec3f5e2f9126
Niklas Q Nielsen <nik@qni.dk>
2020-11-18 16:11:12 -0800

    Ignore invite modal

--- src/orgs/InviteCard.js
+++ src/orgs/InviteCard.js
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -11,6 +10,11 @@ import FirebaseContext from "../util/FirebaseContext";
 import CheckIcon from "@material-ui/icons/Check";
 import ClearIcon from "@material-ui/icons/Clear";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const { v4: uuidv4 } = require("uuid");
 
@@ -21,6 +25,7 @@ export default function InviteCard({ orgID, orgName, setRerender }) {
 
   const [loading, setLoading] = useState(false);
   const [show, setShow] = useState(true);
+  const [ignoreDialogOpen, setIgnoreDialogOpen] = useState();
 
   const linkedTitle = orgID && orgName && (
     <Link style={{ color: "black" }} to={`/orgs/${orgID}`}>
@@ -59,20 +64,7 @@ export default function InviteCard({ orgID, orgName, setRerender }) {
   };
 
   const onIgnore = (e) => {
-    setLoading(true);
-    const ignoreInviteFunc = firebase
-      .functions()
-      .httpsCallable("auth-ignoreInvite");
-
-    ignoreInviteFunc({ orgID: orgID })
-      .then(() => {
-        setShow(false);
-        setRerender(uuidv4());
-      })
-      .catch((err) => {
-        setLoading(false);
-        console.error(err);
-      });
+    setIgnoreDialogOpen(true);
   };
 
   if (!show) {
@@ -80,39 +72,92 @@ export default function InviteCard({ orgID, orgName, setRerender }) {
   }
 
   return (
-    <Card
-      style={{
-        margin: "1rem",
-        borderRadius: "0.5rem",
-        maxHeight: "10rem",
-        width: "20rem",
-      }}
-    >
-      <CardContent>
-        <Typography variant="h6" gutterBottom style={{ fontWeight: "bold" }}>
-          {linkedTitle}
-        </Typography>
-        {loading && <LinearProgress />}
-        <CardActions>
+    <>
+      <Dialog
+        open={ignoreDialogOpen}
+        onClose={() => setIgnoreDialogOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Ignore invite to {orgName}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This is an irreversible action. An administrator will have to invite
+            you again, if you want to join the organization.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
           <Button
-            startIcon={<ClearIcon />}
-            size="small"
             variant="contained"
-            onClick={onIgnore}
+            onClick={() => setIgnoreDialogOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setLoading(true);
+              const ignoreInviteFunc = firebase
+                .functions()
+                .httpsCallable("auth-ignoreInvite");
+
+              ignoreInviteFunc({ orgID: orgID })
+                .then(() => {
+                  setShow(false);
+                  setRerender(uuidv4());
+                })
+                .catch((err) => {
+                  setLoading(false);
+                  console.error(err);
+                });
+
+              setIgnoreDialogOpen(false);
+            }}
+            color="secondary"
+            autoFocus
           >
             Ignore
           </Button>
-          <Button
-            startIcon={<CheckIcon />}
-            size="small"
-            color="secondary"
-            variant="contained"
-            onClick={onAccept}
-          >
-            Accept
-          </Button>
-        </CardActions>
-      </CardContent>
-    </Card>
+        </DialogActions>
+      </Dialog>
+      <Card
+        style={{
+          margin: "1rem",
+          borderRadius: "0.5rem",
+          maxHeight: "10rem",
+          width: "20rem",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h6" gutterBottom style={{ fontWeight: "bold" }}>
+            {linkedTitle}
+          </Typography>
+          {loading && <LinearProgress />}
+          <CardActions>
+            <Button
+              startIcon={<ClearIcon />}
+              size="small"
+              variant="contained"
+              onClick={onIgnore}
+              disabled={loading}
+            >
+              Ignore
+            </Button>
+            <Button
+              startIcon={<CheckIcon />}
+              size="small"
+              color="secondary"
+              variant="contained"
+              onClick={onAccept}
+              disabled={loading}
+            >
+              Accept
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </>
   );
 }
