db6ec9ccc839f7000565021d204c42e8b3498846
Connor Doyle <connor@quantap.com>
2021-01-08 16:42:10 -0800

    Add summaries sidebar nav

--- src/summaries/Summaries.js
+++ src/summaries/Summaries.js
@@ -1,15 +1,29 @@
-import React from "react";
-import Shell from "../shell/Shell.js";
+import React, { useState } from "react";
+import { Search } from "../shell/Search.js";
 import Grid from "@material-ui/core/Grid";
 
 export default function Summaries() {
+
+  // state
+  const [showResults, setShowResults] = useState();
+
+  let searchConfig;
+  if (process.env.REACT_APP_ALGOLIA_SUMMARIES_INDEX) {
+    searchConfig = {
+      index: process.env.REACT_APP_ALGOLIA_SUMMARIES_INDEX,
+      setShowResults: (value) => {
+        setShowResults(value);
+      },
+    };
+  }
+
   return (
-    <Shell>
+    <Search search={searchConfig}>
       <Grid
         container
         className="fullHeight"
         style={{ position: "relative" }}
       ></Grid>
-    </Shell>
+    </Search>
   );
 }
