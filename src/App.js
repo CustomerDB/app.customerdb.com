fc4aabd9d666541420b15477f0963708ac206d3a
Niklas Q Nielsen <nik@qni.dk>
2021-03-18 10:51:03 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Cookies, Privacy, Terms } from "./legal/Legal.js";
 import { Navigate, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 import Admin from "./admin/Admin.js";
@@ -11,7 +12,6 @@ import Login from "./auth/Login.js";
 import Logout from "./auth/Logout.js";
 import Organization from "./organization/Organization.js";
 import Organizations from "./orgs/Organizations.js";
-import React from "react";
 import RequireMembership from "./auth/RequireMembership.js";
 import RequireVerifiedEmail from "./auth/RequireVerifiedEmail.js";
 import ResetPassword from "./auth/ResetPassword.js";
@@ -19,6 +19,58 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Signup from "./auth/Signup.js";
 import Verify from "./auth/Verify.js";
 import WithOauthUser from "./auth/WithOauthUser.js";
+
+function Banner() {
+  let now = new Date();
+  let end = new Date("2021-04-11");
+  let accessMs = end.getTime() - now.getTime();
+  let accessDays = Math.ceil(accessMs / (1000 * 3600 * 24));
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        zIndex: "2147483003",
+        bottom: 0,
+        height: "14rem",
+        width: "100%",
+        backgroundColor: "white",
+        boxShadow:
+          "rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px",
+      }}
+    >
+      <div
+        style={{
+          padding: "2rem",
+        }}
+      >
+        <h4>
+          CustomerDB access ends{" "}
+          {accessDays > 0 ? (
+            <>
+              in {accessDays} day{accessDays > 1 && "s"}.
+            </>
+          ) : (
+            <>very soon!</>
+          )}
+        </h4>
+        <p>
+          We had a blast serving you, but we will unfortunately be shutting down
+          during the coming weeks.
+        </p>
+        <p>
+          Click <a href="/orgs">here</a> to download a copy of your data or
+          click <a href="">here</a> to read more about our closure.
+        </p>
+        <br />
+        <p>
+          Note, that after <b>April 11th</b>, we won't be able to guarantee you
+          can access your data.
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
   const appTheme = createMuiTheme({
@@ -60,93 +112,96 @@ export default function App() {
   });
 
   return (
-    <Router>
-      <ErrorBoundary>
-        <FirebaseContext.Provider value={new Firebase()}>
-          <ThemeProvider theme={appTheme}>
-            <Routes caseSensitive>
-              <Route
-                path="/"
-                element={
-                  <WithOauthUser>
-                    <Login />
-                  </WithOauthUser>
-                }
-              />
-
-              <Route
-                path="login"
-                element={
-                  <WithOauthUser>
-                    <Login />
-                  </WithOauthUser>
-                }
-              />
-
-              <Route path="join">
-                <Route path=":orgID" element={<RedirectToSignup />} />
-              </Route>
-
-              <Route path="signup" element={<Signup />} />
-
-              <Route path="verify" element={<Verify />} />
-
-              <Route path="/reset-password" element={<ResetPassword />} />
-
-              <Route path="orgs">
+    <>
+      <Banner />
+      <Router>
+        <ErrorBoundary>
+          <FirebaseContext.Provider value={new Firebase()}>
+            <ThemeProvider theme={appTheme}>
+              <Routes caseSensitive>
                 <Route
                   path="/"
                   element={
                     <WithOauthUser>
-                      <RequireVerifiedEmail>
-                        <Organizations />
-                      </RequireVerifiedEmail>
+                      <Login />
                     </WithOauthUser>
                   }
                 />
+
                 <Route
-                  path=":orgID/*"
+                  path="login"
+                  element={
+                    <WithOauthUser>
+                      <Login />
+                    </WithOauthUser>
+                  }
+                />
+
+                <Route path="join">
+                  <Route path=":orgID" element={<RedirectToSignup />} />
+                </Route>
+
+                <Route path="signup" element={<Signup />} />
+
+                <Route path="verify" element={<Verify />} />
+
+                <Route path="/reset-password" element={<ResetPassword />} />
+
+                <Route path="orgs">
+                  <Route
+                    path="/"
+                    element={
+                      <WithOauthUser>
+                        <RequireVerifiedEmail>
+                          <Organizations />
+                        </RequireVerifiedEmail>
+                      </WithOauthUser>
+                    }
+                  />
+                  <Route
+                    path=":orgID/*"
+                    element={
+                      <WithOauthUser>
+                        <RequireVerifiedEmail>
+                          <RequireMembership>
+                            <Organization />
+                          </RequireMembership>
+                        </RequireVerifiedEmail>
+                      </WithOauthUser>
+                    }
+                  />
+                </Route>
+
+                <Route
+                  path="admin"
                   element={
                     <WithOauthUser>
                       <RequireVerifiedEmail>
-                        <RequireMembership>
-                          <Organization />
-                        </RequireMembership>
+                        <Admin />
                       </RequireVerifiedEmail>
                     </WithOauthUser>
                   }
                 />
-              </Route>
 
-              <Route
-                path="admin"
-                element={
-                  <WithOauthUser>
-                    <RequireVerifiedEmail>
-                      <Admin />
-                    </RequireVerifiedEmail>
-                  </WithOauthUser>
-                }
-              />
+                <Route path="terms" element={<Terms />} />
 
-              <Route path="terms" element={<Terms />} />
+                <Route path="privacy" element={<Privacy />} />
 
-              <Route path="privacy" element={<Privacy />} />
+                <Route path="cookies" element={<Cookies />} />
 
-              <Route path="cookies" element={<Cookies />} />
+                <Route path="404" element={<Error404 />} />
 
-              <Route path="404" element={<Error404 />} />
+                <Route path="logout">
+                  <Logout />
+                </Route>
 
-              <Route path="logout">
-                <Logout />
-              </Route>
-
-              <Route path="*" element={<Navigate to="/404" />} />
-            </Routes>
-          </ThemeProvider>
-        </FirebaseContext.Provider>
-      </ErrorBoundary>
-    </Router>
+                <Route path="*" element={<Navigate to="/404" />} />
+              </Routes>
+            </ThemeProvider>
+          </FirebaseContext.Provider>
+        </ErrorBoundary>
+      </Router>
+    </>
   );
 }
 
